4748c1a1fffc7e77cb252d7bf63a3ef63ee8e2dd
James Simoes <james@pocostud.io>
2019-01-19 18:50:38 -0500

    add css loader and verify external packages work

--- wp-content/plugins/react-app/client/src/App.js
+++ wp-content/plugins/react-app/client/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
-
+import ReactTable from "react-table";
 import Logo from "./logo.svg";
+import 'react-table/react-table.css'
 
 export class App extends React.Component {
   constructor(props) {
@@ -9,11 +10,43 @@ export class App extends React.Component {
   }
 
   render() {
-    if (this.state.liked) {
-      return <img src={Logo} />;
-    }
+    // if (this.state.liked) {
+    //   return ;
+    // }
 
-    return <button onClick={() => this.setState({ liked: true })}>Like</button>;
+    // return <button onClick={() => this.setState({ liked: true })}>Like</button>;
+
+    const data = [{
+      name: 'Tanner Linsley',
+      age: 26,
+      friend: {
+        name: 'Jason Maurer',
+        age: 23,
+      }
+    }]
+  
+    const columns = [{
+      Header: 'Name',
+      accessor: 'name' // String-based value accessors!
+    }, {
+      Header: 'Age',
+      accessor: 'age',
+      Cell: props => <span className='number'>{props.value}</span> // Custom cell components!
+    }, {
+      id: 'friendName', // Required because our accessor is not a string
+      Header: 'Friend Name',
+      accessor: d => d.friend.name // Custom value accessors!
+    }, {
+      Header: props => <span>Friend Age</span>, // Custom header components!
+      accessor: 'friend.age'
+    }]
+
+    return (
+    <>
+      <img src={Logo} />
+      <ReactTable data={data} columns={columns}/>
+    </>
+    )
   }
 }
 
